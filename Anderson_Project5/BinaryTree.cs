bbe321620b4d450cf6300f98a5d49a60b0aec6bf
Andersonjb1 <91089292+Andersonjb1@users.noreply.github.com>
2022-10-31 16:39:22 -0400

    Initial

---
+++ Anderson_Project5/BinaryTree.cs
@@ -0,0 +1,8 @@
+﻿namespace Anderson_Project5
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Anderson_Project5/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Anderson_Project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                BinaryTree<QuizText> tree = GetTree();
+                BinaryTreeNode<QuizText> node = tree.Root;
+                while (node != null)
+                {
+                    if (node.Left != null || node.Right != null)
+                    {
+                        Console.Write(node.Data.Text);
+                        switch (Console.ReadKey(true).Key)
+                        {
+                            case ConsoleKey.Y:
+                                WriteAnswer(" Yes");
+                                node = node.Left;
+                                break;
+                            case ConsoleKey.N:
+                                WriteAnswer(" No");
+                                node = node.Right;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        WriteAnswer(node.Data.Text);
+                        node = null;
+                    }
+                }
+            }
+        }
+
+        public class QuizText
+        {
+            public string Text { get; set; }
+            public QuizText(string text) => Text = text;
+        }
+
+        private static BinaryTree<QuizText> GetTree()
+        {
+            BinaryTree<QuizText> tree = new BinaryTree<QuizText>();
+            tree.Root = new BinaryTreeNode<QuizText>()
+            {
+                Data = new QuizText("Do you have experience in developing applications ? "),
+
+                Children = new List<TreeNode<QuizText>>()
+                {
+            new BinaryTreeNode<QuizText>()
+            {
+                Data = new QuizText("Have you worked as a developer for more than 5 years ? "),
+                Children = new List<TreeNode<QuizText>>()
+                {
+                    new BinaryTreeNode<QuizText>()
+                    {
+                        Data = new QuizText("Apply as a senior developer!")
+                    },
+                    new BinaryTreeNode<QuizText>()
+                    {
+                        Data = new QuizText("Apply as a middle developer!")
+                    }
+                }
+            },
+            new BinaryTreeNode<QuizText>()
+            {
+                Data = new QuizText("Have you completed the university?"),
+                Children = new List<TreeNode<QuizText>>()
+                {
+                    new BinaryTreeNode<QuizText>()
+                    {
+                        Data = new QuizText("Apply for a junior developer!")
+                    },
+                    new BinaryTreeNode<QuizText>()
+                    {
+                        Data = new QuizText("Will you find some time during the semester?"),
+                        Children = new List<TreeNode<QuizText>>()
+                        {
+                            new BinaryTreeNode<QuizText>()
+                            {
+                                Data = new QuizText("Apply for our long-time internship program!")
+                            },
+                            new BinaryTreeNode<QuizText>()
+                            {
+                                Data = new QuizText("Apply for summer internship program!")
+                            }
+                        }
+                    }
+                }
+            }
+}
+            };
+            tree.Count = 9;
+            return tree;
+        }
+
+        static void WriteAnswer(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
